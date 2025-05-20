0f98b94f2f1f590725c7dd3578dcccac22237c2d
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-20 21:27:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
 provider "oci" {
-<<<<<<< HEAD
-  config_file_profile = "DEFAULT"
-}
-
-data "oci_identity_availability_domains" "ads" {
-  compartment_id = "ocid1.tenancy.oc1..aaaaaaaasiigfpcj7o4xn6o5r725u5zofb5tfmfb57vzqqsirlnkhg6lpiva"
-}
-
-output "availability_domains" {
-  value = data.oci_identity_availability_domains.ads.availability_domains[*].name
-}
-=======
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
@@ -18,10 +6,12 @@ output "availability_domains" {
   region           = var.region
 }
 
-resource "oci_core_instance" "vm1" {}
-resource "oci_core_instance" "vm2" {}
+resource "oci_objectstorage_bucket" "my_bucket" {
+  name           = "placeholder"
+  namespace      = var.namespace
+  compartment_id = var.compartment_id
+}
 
-resource "oci_core_virtual_network" "vcn1" {}
-
-resource "oci_core_subnet" "subnet1" {}
->>>>>>> 1500651354c63d573d7b8a27429b9213e4e87832
+resource "oci_integration_integration_instance" "my_oic" {
+  id = var.oic_ocid
+}
