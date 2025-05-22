3acd0a540c42a6853e0402dde666eb9a36074e7b
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-22 17:51:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,32 @@
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
-}
-
-
 resource "oci_integration_integration_instance" "my_oic" {
-  # placeholder for import
+  compartment_id             = var.compartment_id
+  display_name               = "Test"
+  integration_instance_type  = "ENTERPRISE"
+  is_byol                    = false
+  message_packs              = 1
+  consumption_model          = "UCM"
+  is_file_server_enabled     = true
+  is_visual_builder_enabled  = true
+  shape                      = "PRODUCTION"
+
+  defined_tags = {
+    "Oracle-Tags.CreatedBy" = "default/sajith.m@appsysglobal.com"
+    "Oracle-Tags.CreatedOn" = "2025-04-30T12:36:43.686Z"
+  }
+
+  # Optional: only if you want to recreate or manage DR/VCN/Custom endpoints
+  # idcs_at = var.integration_instance_idcs_at
+  # network_endpoint_details {
+  #   network_endpoint_type = "PUBLIC" # or PRIVATE
+  # }
 }
 
-resource "oci_core_instance" "my_instance" {
-  # placeholder for import
+data "oci_core_instance" "test_instance" {
+  #Required
+  instance_id = oci_core_instance.test_instance.id
 }
 
-resource "oci_database_autonomous_database" "my_atp" {
-  # placeholder for import
+data "oci_database_autonomous_database" "test_autonomous_database" {
+  #Required
+  autonomous_database_id = oci_database_autonomous_database.test_autonomous_database.id
 }
