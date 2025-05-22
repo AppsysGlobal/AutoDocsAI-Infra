eda75718bf9f08a2a6da74cd87183b0f258d60ce
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-22 17:59:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,5 @@
-resource "oci_integration_integration_instance" "my_oic" {
-  compartment_id             = var.compartment_id
-  display_name               = "Test"
-  integration_instance_type  = "ENTERPRISE"
-  is_byol                    = false
-  message_packs              = 1
-  consumption_model          = "UCM"
-  is_file_server_enabled     = true
-  is_visual_builder_enabled  = true
-  shape                      = "PRODUCTION"
+resource "oci_integration_integration_instance" "my_oic" {}
 
-  defined_tags = {
-    "Oracle-Tags.CreatedBy" = "default/sajith.m@appsysglobal.com"
-    "Oracle-Tags.CreatedOn" = "2025-04-30T12:36:43.686Z"
-  }
-
-  # Optional: only if you want to recreate or manage DR/VCN/Custom endpoints
-  # idcs_at = var.integration_instance_idcs_at
-  # network_endpoint_details {
-  #   network_endpoint_type = "PUBLIC" # or PRIVATE
-  # }
-}
 
 data "oci_core_instance" "test_instance" {
   #Required
