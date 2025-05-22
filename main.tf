8c817a67725b1912614b4f7b9fbb0d02a7a7558e
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-22 18:10:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 resource "oci_integration_integration_instance" "my_oic" {}
 
 
+
+
 data "oci_core_instance" "test_instance" {
-  #Required
-  instance_id = oci_core_instance.test_instance.id
+  instance_id = var.compute_instance_id
 }
 
 data "oci_database_autonomous_database" "test_autonomous_database" {
-  #Required
-  autonomous_database_id = oci_database_autonomous_database.test_autonomous_database.id
+  autonomous_database_id = var.atp_database_id
 }
+
